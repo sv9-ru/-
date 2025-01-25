e9b1ca3b65a90907bbd1ee584fb24101b86feb8d
Daria Trofimova <daria@trofimova.net>
2025-01-25 15:42:20 +0300

    mycomplex.h привет к стандартам кодирования

--- mycomplex.h
+++ mycomplex.h
@@ -1,39 +1,68 @@
-#ifndef _MY_COMPLEX_H_
+#ifndef _MY_COMPLEX_H_  // Защита от повторного включения заголовочного файла
 #define _MY_COMPLEX_H_
+
 #include <iostream>
+
 using namespace std;
+
+// Класс Complex, представляющий комплексное число
 class Complex
-{double Re;
-	double Im; 
-	public:
-Complex(double aRe=0, double aIm=0);
-Complex(const Complex&);
-~Complex(    );
-void Set(double aRe, double aIm=0);
-operator double();
- double abs();
- friend istream& operator>>(   istream&,Complex&) ; 
+{
+    private:
+        double Re;
+        double Im;
 
+    public:
+        // Конструктор с параметрами для инициализации комплексного числа
+        Complex(double aRe = 0, double aIm = 0);
 
+        // Конструктор копирования для создания копии комплексного числа
+        Complex(const Complex&);
 
-friend ostream& operator<< (ostream&, Complex & );
+        // Деструктор (не требует дополнительных действий)
+        ~Complex(    );
 
-Complex operator+ (const Complex&);
-Complex operator- (const Complex&);
-Complex operator+ (const double&);
-friend 		Complex operator+ (const double&, const Complex&);
-Complex operator- (const double&);
-friend Complex operator- (const 	double&, const Complex&);
-Complex operator* (const Complex&);
-Complex operator* (const double&);
-friend Complex operator* (const double&, const Complex&)    ;Complex operator/ (const double&);
+        // Метод для установки значений действительной и мнимой части
+        void Set(double aRe, double aIm = 0);
 
-    Complex& operator+= (const Complex&);
-Complex  &  operator-= (const Complex&)    ;
-    Complex& operator*= (const Complex& );  Complex& operator  += (const double&);
-Complex& operator-= (const double&);
-Complex  &operator  *=(const double  & );
-Complex  & operator /=( const   double & );
-Complex& operator = (  const   Complex&);
-Complex & operator= (const double&);};
+        // Оператор преобразования типа для получения модуля комплексного числа
+        operator double();
+
+        // Метод для вычисления модуля (абсолютной величины) комплексного числа
+        double abs();
+
+        // Перегрузка оператора ввода для комплексного числа (считывание данных)
+        friend istream& operator>>(istream&, Complex&) ;
+
+        // Перегрузка оператора вывода для комплексного числа (вывод данных)
+        friend ostream& operator<<(ostream&, Complex&);
+
+        // Операторы для выполнения арифметических операций с комплексными числами
+        Complex operator+(const Complex&);
+        Complex operator-(const Complex&);
+        Complex operator+(const double&);
+        friend Complex operator+(const double&, const Complex&);
+        Complex operator-(const double&);
+        friend Complex operator-(const double&, const Complex&);
+
+        Complex operator*(const Complex&);
+        Complex operator*(const double&);
+        friend Complex operator*(const double&, const Complex&);
+
+        Complex operator/(const double&);
+
+        // Операторы присваивания и модификации комплексного числа
+        Complex& operator+=(const Complex&);
+        Complex& operator-=(const Complex&);
+        Complex& operator*=(const Complex&);
+        Complex& operator+=(const double&);
+        Complex& operator-= (const double&);
+        Complex& operator*=(const double  & );
+        Complex& operator/=( const   double & );
+
+        // Операторы присваивания для копирования значений
+        Complex& operator = (const Complex&);
+        Complex& operator = (const double&);
+};
+
 #endif
